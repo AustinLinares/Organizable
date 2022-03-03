549a95b29f3b7d3119406916bf9fe85d57f4c849
AustinLinares <linaresaustin@hotmail.com>
2022-03-03 10:10:02 -0500

    home page done

---
+++ scripts/pages/home-page.js
@@ -0,0 +1,49 @@
+`
+<section class="biggest-container">
+<aside class="aside">
+  <header class="aside-header vertical-center pd-h">
+    <img src="./assets/icons/organizable.svg" alt="logo">
+  </header>
+  <section class="aside-content ">
+    <div class="aside-div pd-h">
+      <img class="svg" src="./assets/icons/logout-icon.svg" alt="logo">
+      <p>My Boards</p>
+    </div>
+    <div class="aside-div pd-h aside-active">
+      <img class="svg" src="./assets/icons/closed-boards-icon.svg" alt="logo">
+      <p> Closed Boards</p>
+    </div>
+    <div class="aside-div pd-h">
+      <img class="svg" src="./assets/icons/profile-icon.svg" alt="logo">
+      <p>My Profile</p>
+    </div>
+  </section>
+  <footer class="aside-footer pd-h vertical-center">
+    <img class="svg" src="./assets/icons/logout-icon.svg" alt="logo">
+    <a class="anchor" href="#">Log out</a>
+  </footer>
+</aside>
+<main class="window home-container main-padding">
+  <h1 class="heading profile-heading">My Boards</h1>
+  <h2 class="heading heading-sm">Starred Boards</h2>
+  <section class="board-container">
+    <article class="board limon-bc">
+      <p>Board 1</p>
+      <div class="board-footer">
+        <a class="board-link" href="#"><img class="board-footer__img" src="./assets/icons/trash-icon.svg"></a>
+        <a class="board-link" href="#"><img class="board-footer__img" src="./assets/icons/star-active-icon.svg"></a>
+      </div>
+    </article>
+  </section>
+  <h2 class="heading heading-sm">Boards</h2>
+  <section class="board-container">
+    <article class="board limon-bc">
+      <p>Board 1</p>
+      <div class="board-footer">
+        <a class="board-link" href="#"><img class="board-footer__img" src="./assets/icons/trash-icon.svg"></a>
+        <a class="board-link" href="#"><img class="board-footer__img" src="./assets/icons/star-desactive-icon.svg"></a>
+      </div>
+    </article>
+  </section>
+</main>
+</section>`
--- scripts/pages/profile-page.js
+++ scripts/pages/profile-page.js
@@ -1,4 +1,4 @@
-`        <section class="biggest-container">
+`            <section class="biggest-container">
 <aside class="aside">
   <header class="aside-header vertical-center pd-h">
     <img src="./assets/icons/organizable.svg" alt="logo">
@@ -24,17 +24,19 @@
 </aside>
 <main class="window home-container main-padding">
   <h1 class="heading profile-heading">My Profile</h1>
-  <form class="login-form">
-    <label class="label" for="username">USERNAME</label>
-    <input class="input" id="username" placeholder="placeholder" type="text">
-    <label class="label" for="email">EMAIL</label>
-    <input class="input" id="email" placeholder="placeholder" type="password">
-    <label class="label" for="fname">FIRST NAME</label>
-    <input class="input" id="fname" placeholder="placeholder" type="password">
-    <label class="label" for="lname">LAST NAME</label>
-    <input class="input" id="lname" placeholder="placeholder" type="password">
-    <button class="submit bc-pink" type="submit">UPDATE PROFILE</button>
-  </form>
-  <a class="delete-acc submit bc-gray vertical-center h-c" href="#">DELETE MY ACCOUNT</a>
+  <section class="horazontal-center">
+     <form class="login-form">
+      <label class="label" for="username">USERNAME</label>
+      <input class="input max-width" id="username" placeholder="placeholder" type="text">
+      <label class="label" for="email">EMAIL</label>
+      <input class="input max-width" id="email" placeholder="placeholder" type="password">
+      <label class="label" for="fname">FIRST NAME</label>
+      <input class="input max-width" id="fname" placeholder="placeholder" type="password">
+      <label class="label" for="lname">LAST NAME</label>
+      <input class="input max-width" id="lname" placeholder="placeholder" type="password">
+      <button class="submit max-width bc-pink" type="submit">UPDATE PROFILE</button>
+    </form>
+    <a class="delete-acc max-width submit bc-gray vertical-center h-c" href="#">DELETE MY ACCOUNT</a>
+  </section>
 </main>
-</section>`
+</section>>`
