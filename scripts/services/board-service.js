2c654b9e5de87ae5cd9c6810bd7f49e1fafc4481
AustinLinares <linaresaustin@hotmail.com>
2022-03-03 16:39:46 -0500

    requests to boards done

--- scripts/services/board-service.js
+++ scripts/services/board-service.js
@@ -5,6 +5,10 @@ export async function getBoards() {
   return await apiFetch("boards");
 };
 
+export async function showBoard(id) {
+  return await apiFetch("boards/" + id);
+};
+
 export async function createBoard( newBoard = { name, color }) {
   return await apiFetch("boards", {body: newBoard });
 };
@@ -13,7 +17,8 @@ export async function deleteBoard(id) {
   return await apiFetch("boards/" + id, {method: "DELETE" });
 };
 
-export async function editNoard(id, newContact = { name, color, starred, closed }) {
+
+export async function editBoard(id, newContact = { name, color, starred, closed }) {
   return await apiFetch("boards/" + id, {method: "PATCH", body: newContact});
 };
 
@@ -24,10 +29,50 @@ export async function editNoard(id, newContact = { name, color, starred, closed 
 // }).then(getBoards).then(console.log)//.then(logout);
 
 // test createBoard
-// const user = login({
-// 	username: "user24",
-// 	password: "123456"
-// }).then(createBoard({
-//   name: "created from test",
-//   color: "red",
-// })).then(console.log)//.then(logout);
+// async function testCreateBoard() {
+//   const user = await login({
+//     username: "user24",
+//     password: "123456"
+//   })
+//   createBoard({
+//     name: "created from test",
+//     color: "red",
+//   }).then(console.log)
+// }
+// testCreateBoard();
+
+// test createBoard
+// async function testDeleteBoard() {
+//   const user = await login({
+//     username: "user24",
+//     password: "123456"
+//   })
+//   deleteBoard(385).then(console.log)
+// }
+// testDeleteBoard();
+
+// test updateBoard
+// async function testUpdateBoard() {
+//   const user = await login({
+//     username: "user24",
+//     password: "123456"
+//   })
+//   editBoard(383, {
+//     name: "created from test2",
+//     color: "red",
+//     starred: true,
+//   }).then(console.log)
+// }
+// testUpdateBoard();
+
+// test updateBoard
+// async function testGetBoard() {
+//   const user = await login({
+//     username: "user24",
+//     password: "123456"
+//   })
+//   showBoard(383).then(console.log)
+// }
+// testGetBoard();
+
+
