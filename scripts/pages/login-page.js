167a0f2128fc04d954c66c4231683f013e1123d6
AustinLinares <linaresaustin@hotmail.com>
2022-03-02 20:22:36 -0500

    some templates created

---
+++ scripts/pages/login-page.js
@@ -0,0 +1,12 @@
+`<section class="window main-container">
+<img src="./assets/icons/organizable.svg" alt="logo">
+<h2 class="heading">Login</h2>
+<form class="login-form">
+  <label class="label" for="username">USERNAME</label>
+  <input class="input" id="username" placeholder="placeholder" type="text">
+  <label class="label" for="password">PASSWORD</label>
+  <input class="input" id="password" placeholder="placeholder" type="password">
+  <button class="submit" type="submit">LOGIN</button>
+</form>
+<a class="anchor" href="#">Create Account</a>
+</section>`
---
+++ scripts/pages/signup-page.js
@@ -0,0 +1,18 @@
+`<section class="window main-container">
+<img src="./assets/icons/organizable.svg" alt="logo">
+<h2 class="heading">Create Account</h2>
+<form class="login-form">
+  <label class="label" for="username">USERNAME</label>
+  <input class="input" id="username" placeholder="placeholder" type="text">
+  <label class="label" for="email">EMAIL</label>
+  <input class="input" id="email" placeholder="placeholder" type="password">
+  <label class="label" for="fname">FIRST NAME</label>
+  <input class="input" id="fname" placeholder="placeholder" type="password">
+  <label class="label" for="lname">LAST NAME</label>
+  <input class="input" id="lname" placeholder="placeholder" type="password">
+  <label class="label" for="password">PASSWORD</label>
+  <input class="input" id="password" placeholder="placeholder" type="password">
+  <button class="submit" type="submit">CREATE ACCOUNT</button>
+</form>
+<a class="anchor" href="#">Login</a>
+</section>`
