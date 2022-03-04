f104bdea280aa7c09b1167dde280884dea7ac8c9
AustinLinares <linaresaustin@hotmail.com>
2022-03-04 09:10:42 -0500

    module for closed boards almost done

---
+++ scripts/components/closed-board.js
@@ -0,0 +1,45 @@
+import STORE from "../store.js";
+
+
+function renderClosedBoard(closedBoard) {
+  console.log(closedBoard);
+  return `
+  <article class="board limon-bc">
+    <p>Board 1</p>
+    <div class="board-footer">
+      <a class="board-link" href="#"><img class="board-footer__img" src="./assets/icons/recover-icon.svg"></a>
+      <a class="board-link" href="#"><img class="board-footer__img" src="./assets/icons/trash-icon.svg"></a>
+    </div>
+  </article>
+`;
+}
+
+function render() {
+  let closedBoards = JSON.parse(localStorage.getItem("closed")) || STORE.closed;
+  return `
+  <section class="board-container">
+    ${closedBoards.map(renderClosedBoard).join("")}
+  </section>
+  `;
+}
+
+const BoardsClosed = {
+  toString() {
+    return render();
+  },
+  addListeners() {
+  },
+};
+
+export default BoardsClosed;
+
+// `
+// <section class="board-container">
+//   <article class="board limon-bc">
+//     <p>Board 1</p>
+//     <div class="board-footer">
+//       <a class="board-link" href="#"><img class="board-footer__img" src="./assets/icons/recover-icon.svg"></a>
+//       <a class="board-link" href="#"><img class="board-footer__img" src="./assets/icons/trash-icon.svg"></a>
+//     </div>
+//   </article>
+// </section>`
